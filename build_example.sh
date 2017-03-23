0b33966c55c143d815608f32375b1b2ca352d53a
ISHII Tsuubito <ishii@buffer-renaiss.com>
2017-03-23 11:27:10 +0900

    update script

--- build_example.sh
+++ build_example.sh
@@ -2,5 +2,6 @@
 
 for source in simple customize;
 do
-    g++ $1 -I./include -std=c++11 -stdlib=libc++ -o example/$source example/$source.cpp && echo build complete: example/$source
+    echo build start: example/$source
+    g++ $1 -I./include -std=c++11 -stdlib=libc++ -o example/$source example/$source.cpp && echo build complete: example/$source || echo build failed: example/$source
 done
